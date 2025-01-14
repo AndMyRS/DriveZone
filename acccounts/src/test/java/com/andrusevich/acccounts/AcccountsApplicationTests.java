ee8e37b0b2274592dad3e08ca25754b982f0e43d
Vladimir Andrusevich <vl.andrusevich@yandex.by>
2025-01-14 17:25:59 +0300

    Initial commit

---
+++ acccounts/src/test/java/com/andrusevich/acccounts/AcccountsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andrusevich.acccounts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcccountsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
